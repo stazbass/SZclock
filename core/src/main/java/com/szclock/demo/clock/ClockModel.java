7f802a35989a04cd4a5287926a71f82cdbe4e891
stazbass <stazbass@gmail.com>
2018-03-21 14:19:14 +0200

    Seconds upgrade - anims

--- core/src/main/java/com/szclock/demo/clock/ClockModel.java
+++ core/src/main/java/com/szclock/demo/clock/ClockModel.java
@@ -22,22 +22,25 @@ public class ClockModel {
         long milliseconds = record.getMilliseconds();
         List<Vector2> result = new ArrayList<>(70);
 
+        System.out.println(secondsNow);
         for (int secondIterator = 0; secondIterator < secondsNow; secondIterator++) {
             Vector2 position = getSecondPointPosition(origin, scale, secondIterator);
             result.add(position);
         }
 
-        long millisecondsInSecond = milliseconds % 1000;
-        double dtime = millisecondsInSecond <= 500 ? millisecondsInSecond / 500.0 : (1000 - millisecondsInSecond) / 500.0;
-        double currentSecondArrowScale = Interpolation.smoother.apply((float) dtime);
 
-        for (int j = 1; j <= secondsNow; j++) {
-            for (int secondIterator = 0; secondIterator < j * currentSecondArrowScale; secondIterator++) {
-//                result.add(getSecondPointPosition(origin, scale, secondIterator, milliseconds));
-            }
+        long millisecondsInSecond = milliseconds % 1000;
+        double dtime = millisecondsInSecond / 1000.0;
+        double currentSecondArrowScale = Interpolation.smoother.apply((float) dtime);
+        if(secondsNow == 59){
+            result.stream().forEach(res -> res.interpolate(origin, (float)dtime, Interpolation.smoother));
         }
-//        Vector2 millisPosition = getMillisecondsPosition(origin, millisecondsInSecond, secondsNow, 1.0);
-//        result.add(millisPosition);
+
+
+        Vector2 position = getSecondPointPosition(origin, scale, secondsNow*currentSecondArrowScale);
+
+        result.add(position);
+
         return result;
     }
 
@@ -57,24 +60,9 @@ public class ClockModel {
         return getPointOnCircle(origin.x, origin.y, degree, scale * minute);
     }
 
-    private Vector2 getSecondPointPosition(Vector2 origin, double scale, long secondsNow) {
-        double secondsArrowDegree = (double) secondsNow;
+    private Vector2 getSecondPointPosition(Vector2 origin, double scale, double secondsNow) {
+        double secondsArrowDegree =  secondsNow;
         float degree = (float) (Math.PI / 2.0 - secondsArrowDegree * Math.PI * 2.0 / 60.0);
         return getPointOnCircle(origin.x, origin.y, degree, scale * secondsNow);
     }
-
-    private Vector2 getMillisecondsPosition(Vector2 origin, long milliseconds, long secondsNow, double scale) {
-        float millisecondsNormalized = (1 - milliseconds / 1000.0f);
-        Vector2 secondsPosition = getSecondPointPosition(origin, scale, secondsNow);
-        Vector2 nextSecondsPosition = getSecondPointPosition(origin, scale, secondsNow + 1);
-        Vector2 millisPosition = secondsPosition.interpolate(nextSecondsPosition, millisecondsNormalized, getInterpolation());
-        return millisPosition;
-    }
-
-
-    private Interpolation getInterpolation() {
-        return Interpolation.smooth;
-    }
-
-
 }
