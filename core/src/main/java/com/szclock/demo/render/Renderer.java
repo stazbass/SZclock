cc78046e9010ba67e05467472254f1ed4f002912
stazbass <stazbass@gmail.com>
2018-03-30 11:37:37 +0300

    renderer alpha

--- core/src/main/java/com/szclock/demo/render/Renderer.java
+++ core/src/main/java/com/szclock/demo/render/Renderer.java
@@ -1,5 +1,6 @@
 package com.szclock.demo.render;
 
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.szclock.demo.entities.RenderItem;
@@ -17,6 +18,8 @@ public class Renderer {
     public void render(Supplier<SpriteBatch> batchSupplier, Supplier<List<RenderItem>> spriteSupplier) {
         SpriteBatch spriteBatch = batchSupplier.get();
         spriteBatch.begin();
+        spriteBatch.enableBlending();
+        spriteBatch.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE);
         spriteSupplier.get().stream().forEach(renderAtom -> {
             Texture texture = textureManager.loadTexture(renderAtom.getTexture());
             spriteBatch.draw(texture, renderAtom.getPosition().x - texture.getWidth() / 2.0f * renderAtom.getScale().x, renderAtom.getPosition().y - texture.getHeight() / 2.0f * renderAtom.getScale().y,
